9c20505980d372e68a9fd4f20d4447d1c1006dbb
Mauricio Plaza <mauricioplaza0378@gmail.com>
2024-10-12 14:28:55 -0500

    Egresos

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import './pages/Egresos.css'
 
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import Home from './pages/Home'
 import './pages/Home.css';
+import Egresos from './pages/Egresos'
 
 let router = createBrowserRouter([
   {
@@ -20,6 +22,10 @@ let router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/Egresos',
+    element: <Egresos />
   }
 ])
 
